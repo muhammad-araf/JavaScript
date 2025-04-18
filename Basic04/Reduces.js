98ac163788a57b8485ee9279446738811e109909
m-araf <lentoaraf@gmail.com>
2025-04-19 00:15:53 +0500

    Sum of Prices of API's

--- Basic04/Reduces.js
+++ Basic04/Reduces.js
@@ -1,4 +1,25 @@
-const arr = [1,2,3,4,5,6,7,8,9,10];
-//1+2+3+4+...+10
-const sum = arr.reduce((acc,curr) => (acc+curr),0);
-console.log(sum);
+// const arr = [1,2,3,4,5,6,7,8,9,10];
+// //1+2+3+4+...+10
+// const sum = arr.reduce((acc,curr) => (acc+curr),0);
+// console.log(sum);
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+const Sum = shoppingCart.reduce( (sum,i) => (sum+i.price),0);
+console.log(Sum);
