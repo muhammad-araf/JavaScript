8bb633e432b2de88a050982623688a8423793643
m-araf <lentoaraf@gmail.com>
2025-04-18 17:24:15 +0500

    ForEach with array Object

--- Basic04/Iteration.js
+++ Basic04/Iteration.js
@@ -1,11 +1,30 @@
-const arr = [1,2,3,4,5,6]
-// arr2 = ""
-for(i of arr){
-    // arr2 += i + " "
-    console.log(i);
-}
+// const arr = [1,2,3,4,5,6]
+//     arr2 = ""
+// for(i of arr){
+//     arr2 += i + " "
+//     console.log(i);
+// }
 // console.log(arr2)
 
-arr.forEach( (item) =>{
-    console.log(item);
-}  )
+// arr.forEach( (item) =>{
+//     console.log(item);
+// }  )
+
+const arr3 = [
+    {
+        name: "Araf",
+        age: 17
+    },
+    {
+        name: "Maham",
+        age: 99
+    },
+    {
+        name: "Fasih",
+        age: 20
+    }
+]
+
+arr3.forEach((item)=>{
+    console.log(`Name: ${item.name} \t\t Age: ${item.age}`);
+})
