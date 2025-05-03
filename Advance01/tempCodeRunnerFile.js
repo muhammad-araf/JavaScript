79974595f2b9866c0f963d3425a7fe8bbd361f6f
muhammad-araf <lentoaraf@gmail.com>
2025-05-03 10:26:38 +0500

    delete temp file

--- Advance01/tempCodeRunnerFile.js
+++
@@ -1,5 +0,0 @@
-(async function getUserData() {
-    const response = await fetch('https://api.github.com/users/muhammad-araf');
-    const data = await response.json();
-    console.log(data);
-})();
