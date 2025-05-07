4a0abe8eba0d57a8c5148c7f49e6560e960ab6da
muhammad-araf <lentoaraf@gmail.com>
2025-05-07 06:02:42 +0500

    OOP -constructor -object -encapsulation

--- Advance02/ObjectOrientedProgramming.js
+++ Advance02/ObjectOrientedProgramming.js
@@ -1,10 +1,29 @@
-function user(name,id){
-    this.name = name;
-    this.id = id;
-    this.print = function(marks){
-        console.log(`name : ${name}\n Id : ${id}\n marks : ${marks}`);
+// function user(name,id){
+//     this.name = name;
+//     this.id = id;
+//     this.print = function(marks){
+//         console.log(`name : ${name}\n Id : ${id}\n marks : ${marks}`);
         
+//     }
+// }
+// const student = new user("araf",991);
+// student.print(90);
+
+// Define a class (OOP-style)
+class Car {
+    constructor(brand, model) {
+        this.brand = brand;
+        this.model = model;
+    }
+
+    displayInfo() {
+        console.log(`This car is a ${this.brand} ${this.model}`);
+
     }
 }
-const student = new user("araf",991);
-student.print(90);
+
+let car1 = new Car("Toyota", "Corolla");
+let car2 = new Car("Honda", "Civic");
+
+    car1.displayInfo();
+    car2.displayInfo();
